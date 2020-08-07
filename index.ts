457d563403967331335db13abce00a1f247b1f2b
Raphael <raph-topo@posteo.net>
2020-08-07 16:10:14 +0200

    Copy required TopoJSON to `dist`
    Adapt relevant import paths.

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import type {Topology} from 'topojson-specification'
 
-import land50mJSON from './world/50m.json'
+import land50mJSON from './dist/50m.json'
 export let land50m: Topology = (land50mJSON as any) as Topology
 
-import land110mJSON from './world/110m.json'
+import land110mJSON from './dist/110m.json'
 export let land110m: Topology = (land110mJSON as any) as Topology
