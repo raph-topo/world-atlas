2c92967d9527523dbc9954c6588cc2992b964913
Raphael <raph-topo@posteo.net>
2020-08-08 09:06:05 +0200

    Rename variables and exports

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import type {Topology} from 'topojson-specification'
 
-import land50mJSON from './world/50m.json'
-export let land50m: Topology = (land50mJSON as any) as Topology
+import world50mJSON from './world/50m.json'
+export let world50m: Topology = (world50mJSON as any) as Topology
 
-import land110mJSON from './world/110m.json'
-export let land110m: Topology = (land110mJSON as any) as Topology
+import world110mJSON from './world/110m.json'
+export let world110m: Topology = (world110mJSON as any) as Topology
